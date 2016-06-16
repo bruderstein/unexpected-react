adf97af0a51ef4a43c9cbb2341b7aac6426da712
Sune Simonsen <sune@we-knowhow.dk>
2016-06-16 20:39:30 +0200

    Made npm script for running documentation tests

--- bootstrap-unexpected-markdown.js
+++ bootstrap-unexpected-markdown.js
@@ -1,4 +1,65 @@
-/*global unexpected:true, sinon:true*/
-unexpected = require('unexpected');
+/*global unexpected:true, TestUtils:true, React:true, sinon:true*/
+
+require( './src/testHelpers/emulateDom');
+
+global.unexpected = require('unexpected');
 unexpected.output.preferredWidth = 80;
-unexpected.installPlugin(require('./lib/unexpected-react'));
+unexpected.use(require('./src/unexpected-react'));
+
+global.TestUtils = require('react-addons-test-utils');
+global.React = require('react');
+
+
+const TodoItem = React.createClass({
+    propTypes: {
+        id: React.PropTypes.number,
+        label: React.PropTypes.string
+    },
+
+    getInitialState() {
+        return {
+            isCompleted: 'false'
+        };
+    },
+
+    onClick() {
+        this.setState({
+            isCompleted: 'true'
+        });
+    },
+
+    render() {
+        return (
+            <div className="item">
+                <span className="id">{this.props.id}</span>
+                <span>{this.props.label}</span>
+                <span>Is complete {this.state.isCompleted}</span>
+                <button onClick={this.onClick}>Click me</button>
+            </div>
+        );
+    }
+});
+
+const TodoList = React.createClass({
+    propTypes: {
+        children: React.PropTypes.node
+    },
+
+    render() {
+        const { children } = this.props;
+
+        return (
+            <div>
+                <div className="items">
+                    { children }
+                </div>
+                <div className="add-new-item">
+                   <input placeholder="Enter something to do" />
+                </div>
+            </div>
+        );
+    }
+});
+
+global.TodoItem = TodoItem;
+global.TodoList = TodoList;
