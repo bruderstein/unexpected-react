d23d7c078cc43a5747426a57939f071616d369dc
Dave Brotherstone <davegb@pobox.com>
2016-12-04 10:54:19 +0100

    Add regression spec for #34
    This was actually due to a bug in an earlier version, but adding the
    spec as another real world example of the regression.

---
+++ src/tests/issue34.spec.js
@@ -0,0 +1,106 @@
+import unexpectedReact from '../unexpected-react';
+import React, { Component } from 'react';
+import { createRenderer } from 'react-addons-test-utils';
+import unexpected from 'unexpected';
+
+class Foo extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      focus: false
+    };
+    
+    this.handleFocus = this.handleFocus.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+  }
+  
+  handleFocus(e) {
+    this.setState({ focus: true });
+  }
+  
+  handleBlur(e) {
+    this.setState({ focus: false });
+  }
+  
+  render() {
+    const {
+      focus
+    } = this.state;
+    
+    return (
+      <div tabIndex='0' onFocus={this.handleFocus} onBlur={this.handleBlur}>
+        {focus &&
+        <div className='with-focus'>
+          <span>with focus</span>
+        </div>
+        }
+      </div>
+    );
+  }
+}
+
+const expect = unexpected
+  .clone()
+  .use(unexpectedReact);
+
+describe('issue 34', function () {
+  let renderer;
+  
+  before(() => {
+    expect.addAssertion('<ReactShallowRenderer> when focussed <assertion>', (expect, renderer) => {
+      const { props: { onFocus } } = renderer.getRenderOutput();
+      onFocus();
+      return expect.shift(renderer);
+    });
+    expect.addAssertion('<ReactShallowRenderer> when blurred <assertion>', (expect, renderer) => {
+      const { props: { onBlur } } = renderer.getRenderOutput();
+      onBlur();
+      return expect.shift(renderer);
+    });
+  });
+  
+  beforeEach(() => {
+    renderer = createRenderer();
+  });
+  
+  describe('with event', function () {
+    it(`renders div.with-focus on focus`, function () {
+      renderer.render(<Foo />);
+      
+      return expect(renderer, 'with event focus', 'to contain', (
+        <div className='with-focus'>
+          <span>with focus</span>
+        </div>
+      ));
+    });
+    
+    it(`does not render div.with-focus' on blur`, function () {
+      renderer.render(<Foo />);
+      
+      return expect(renderer, 'with event focus', 'with event blur', 'not to contain', (
+        <div className='with-focus' />
+      ));
+    });
+  });
+  
+  describe('custom', function () {
+    it(`renders div.with-focus on focus`, function () {
+      renderer.render(<Foo />);
+      
+      return expect(renderer, 'when focussed', 'to contain', (
+        <div className='with-focus'>
+          <span>with focus</span>
+        </div>
+      ));
+    });
+    
+    it(`does not render div.with-focus on blur`, function () {
+      renderer.render(<Foo />);
+      
+      return expect(renderer, 'when focussed', 'when blurred', 'not to contain', (
+        <div className='with-focus' />
+      ));
+    });
+  });
+});
